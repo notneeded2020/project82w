d79d35091cefc70be96f5845962356b57c04d737
notneeded2020 <75374552+notneeded2020@users.noreply.github.com>
2021-11-16 09:18:20 +0530

    Update PostCard.js

--- screens/PostCard.js
+++ screens/PostCard.js
@@ -1,81 +1,116 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, Platform } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
-import Ionicons from "react-native-vector-icons/Ionicons"
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default class PostCard extends React.Component(){
-
-   constructor(props) {
-      super(props);
-      this.state = {};
+export default class PostCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
 
-  componentDidMount() { }
-  
-    render(){
-        return(
-         <View style={styles.container}>
-            <View style={styles.cardContainer}>
-                <View>
-                    <View>
-                      <Image
-                         source={require("../assets/profile_img.png")}
-                      >
-                      </Image>
-                    </View>
-                    <View>
-                        <Text>{this.props.post.author}</Text>
-                    </View> 
-                </View>
-                <Image source={require("../assets/post.jpeg")}/>
-                <View>
-                 <Text>
-                    {this.props.post.caption}
-                 </Text>
-                </View>
-                <View>
-                   <View>
-                        <Ionicons name={"heart"} size={RFValue (30)} color={"white"}/>
-                        <Text>12k</Text>
-                   </View>
-                </View>
+  componentDidMount() {}
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.cardContainer}>
+          <View style={styles.authorContainer}>
+            <View style={styles.authorImageContainer}>
+              <Image
+                source={require('../assets/profile_img.png')}
+                style={styles.profileImage}></Image>
             </View>
+            <View style={styles.authorNameContainer}>
+              <Text style={styles.authorNameText}>
+                {this.props.post.author}
+              </Text>
+            </View>
+          </View>
+          <Image
+            source={require('../assets/post.jpeg')}
+            style={styles.postImage}
+          />
+          <View style={styles.captionContainer}>
+            <Text style={styles.captionText}>{this.props.post.captions}</Text>
+          </View>
+          <View style={styles.actionContainer}>
+            <View style={styles.likeButton}>
+              <Ionicons name={'heart'} size={RFValue(30)} color={'white'} />
+              <Text style={styles.likeText}>12k</Text>
+            </View>
+          </View>
         </View>
-        )
-    }
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container:{
-       flex:1,
-       backgroundColor: "black"
-    },
-    droidSafeArea: {
-       marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
-    },
-    appTitle:{
-       flex:0.07,
-       flexDirection: "row"
-    },
-    appIcon: {
-       flex:0.2,
-       justifyContent:"center",
-       alignItems:"center"
-    },
-    iconImage:{
-      width:"100%",
-      height:"100%",
-      resizeMode:"contain"
-    },
-    appTitleTextContainer:{
-       flex:0.8,
-       justifyContent:"center"
-    },
-    appTitleText:{
-       color:"white",
-       fontSize: RFValue(28)
-    },
-    cardContainer:{
-        flex:0.85
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  droidSafeArea: {
+    marginTop:
+      Platform.OS === 'android' ? StatusBar.currentHeight : RFValue(35),
+  },
+  cardContainer: {
+    margin: RFValue(13),
+    backgroundColor: '#2f345d',
+    borderRadius: RFValue(20),
+  },
+  authorContainer: {
+    margin: RFValue(13),
+    backgroundColor: '#2f345d',
+    borderRadius: RFValue(20),
+  },
+  profileImage: {
+    flex: 0.3,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  postImage: {
+    resizeMode: 'contain',
+    width: '95%',
+    alignSelf: 'center',
+    height: RFValue(250),
+  },
+  likeButton: {
+    width: RFValue(165),
+    height: RFValue(40),
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#eb3948',
+    borderRadius: RFValue(30),
+  },
+  actionContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: RFValue(10),
+  },
+  authorNameContainer: {
+    paddingLeft: RFValue(0),
+    justifyContent: 'center',
+    alignItems:"center"
+  },
+  authorNameText: {
+    fontSize: RFValue(18),
+    color: 'white',
+  },
+  captionContainer: {
+    paddingLeft: RFValue(0),
+    justifyContent: 'center',
+    alignItems:"center"
+  },
+  captionText: {
+    fontSize: 13,
+    color: 'white',
+    paddingTop: RFValue(10),
+  },
+   likeText: {
+    color: 'white',
+    fontSize: RFValue(25),
+    marginLeft: RFValue(5),
+  },
+});
